0f32784af092cbda6c3582dbc8d067140be9020e
AbhishekChamp <abhishek96.r@gmail.com>
2023-01-01 00:59:32 +0530

    Day - 01

---
+++ 03_Routers/router/blog_get.py
@@ -0,0 +1,43 @@
+from fastapi import status, Response, APIRouter
+from enum import Enum
+from typing import Optional
+
+router = APIRouter(prefix='/blog', tags=['blog'])
+
+@router.get('/all', tags=['blog'], summary='Retrieve all blogs', description='This api call simulates fetching all blogs.', response_description='List of available blogs.')
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
+
+
+@router.get('/{id}/comments/{comment_id}', tags=['blog', 'comment'])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+
+    """
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+
+class BlogType(str, Enum):
+    short= 'short'
+    story= 'story'
+    howto= 'howto'
+
+
+@router.get('/type/{type}', tags=['blog'])
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type {type}'}
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id:int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f"Blog id is {id}"}
---
+++ README.md
@@ -0,0 +1,21 @@
+## Learn everything about FastApi with Python, Full Stack, OAuth2, SQLAlchemy, RESTful APIs, and practice projects
+
+An udemy course for learning FastApi which is a python framework which is used for backend web development.
+
+In this course I learnt about,
+
+1. Get Methods
+2. Operation Description
+3. Routers
+4. Parameters
+5. Database with SQLAlchemy
+6. Custom Response, Cookies, Headers
+7. Authentication
+8. Working with files
+9. Tasks
+10. Async await, templates, middleware
+11. Dependencies
+12. OCR Application
+13. Blog Site - ReactJS, FastAPI
+14. Instagram - React JS, FastAPI, Android
+15. Warehouse app - Microservices and Redis
