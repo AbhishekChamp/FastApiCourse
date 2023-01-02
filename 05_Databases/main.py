a5cb21f542f57c013d055b6bc0e109e3997b973a
AbhishekChamp <abhishek96.r@gmail.com>
2023-01-02 23:49:07 +0530

    Day-03

---
+++ 05_Databases/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from router import blog_get, blog_post, user
+from db import models
+from db.database import engine
+
+app = FastAPI()
+app.include_router(user.router)
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+
+@app.get('/hello')
+def index():
+    return {'message': 'Hello World!'}
+
+models.Base.metadata.create_all(engine)
