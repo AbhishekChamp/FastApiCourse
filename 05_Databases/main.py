b7dfb054305a88e9a807734e3947dca98d40d945
AbhishekChamp <abhishek96.r@gmail.com>
2023-01-03 19:19:53 +0530

    Day - 04

--- 05_Databases/main.py
+++ 05_Databases/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from router import blog_get, blog_post, user
+from router import blog_get, blog_post, user, article
 from db import models
 from db.database import engine
 
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
 
 @app.get('/hello')
 def index():
